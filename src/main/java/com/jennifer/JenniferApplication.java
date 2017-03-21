5c88555d71c11b69f5abbb9fb41fe2a3107fada7
James Ho <jamesho287@gmail.com>
2017-03-21 20:13:16 +0200

    2nd test with heroku

--- src/main/java/com/jennifer/JenniferApplication.java
+++ src/main/java/com/jennifer/JenniferApplication.java
@@ -5,17 +5,24 @@ import com.jennifer.service.properties.StorageProperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 @ComponentScan("com.jennifer.controller")
-public class JenniferApplication {
+public class JenniferApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenniferApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenniferApplication.class);
 	}
 
 	@Bean
